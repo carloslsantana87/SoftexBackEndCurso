2031fa7af722bf969bf8c69fef0d2c0f81dc0ea7
carloslsantana87 <139463823+carloslsantana87@users.noreply.github.com>
2023-11-20 17:30:13 -0300

    Atualizacao

--- JavaScript/Estrutura e Variáveis em Java Script/Calculadoraoperadorvariavel.js
+++ JavaScript/Estrutura e Variáveis em Java Script/Calculadoraoperadorvariavel.js
@@ -13,7 +13,7 @@ console.log("ESCOLHA O OPERADOR PARA REALIZAR O CALCULO!");
 s = prompt("Digite o operador: + para SOMA, - para Subtração, * para Multiplicação, / para Divisão:");
 
 if (s === "/") {
- 
+
 
     if (eval(num1 % num2) == 0) {
 
@@ -21,8 +21,8 @@ if (s === "/") {
         console.log(`O resultado da operação é: ${result}, sem resto`);
 
     } else {
-         
-        result = eval(num1 +  s + num2);
+
+        result = eval(num1 + s + num2);
         restante = (eval(num1 + s + num2)) % num2;
         console.log(`O resultado da divisão é: ${result.toFixed(2)}`);
         console.log(`O resto da divisão é: ${restante.toFixed(2)}`);
@@ -31,6 +31,6 @@ if (s === "/") {
 
 } else {
 
-    result = eval(num1 +  s + num2);
+    result = eval(num1 + s + num2);
     console.log(`O resultado da operação é: ${result}`);
 }
--- JavaScript/Estrutura e Variáveis em Java Script/Calculadoraoperadorvariavel.ts
+++ JavaScript/Estrutura e Variáveis em Java Script/Calculadoraoperadorvariavel.ts
@@ -1,6 +1,6 @@
 const prompt = require('prompt-sync')();
-var s:symbol = Symbol();
-var num1:number, num2:number, result:number, restante:number;
+var s: symbol = Symbol();
+var num1: number, num2: number, result: number, restante: number;
 
 console.log("BEM-VINVO A CALCULADORA!");
 console.log("DIGITE OS NÚMEROS E ESCOLHA O OPERADOR PARA REALIZAR O CÁLCULO!")
@@ -13,7 +13,7 @@ console.log("ESCOLHA O OPERADOR PARA REALIZAR O CALCULO!");
 s = prompt("Digite o operador: + para SOMA, - para Subtração, * para Multiplicação, / para Divisão:");
 
 if (s === "/") {
- 
+
 
     if (eval(num1 % num2) == 0) {
 
@@ -21,8 +21,8 @@ if (s === "/") {
         console.log(`O resultado da operação é: ${result}, sem resto`);
 
     } else {
-         
-        result = eval(num1 +  s + num2);
+
+        result = eval(num1 + s + num2);
         restante = (eval(num1 + s + num2)) % num2;
         console.log(`O resultado da divisão é: ${result.toFixed(2)}`);
         console.log(`O resto da divisão é: ${restante.toFixed(2)}`);
@@ -31,6 +31,6 @@ if (s === "/") {
 
 } else {
 
-    result = eval(num1 +  s + num2);
+    result = eval(num1 + s + num2);
     console.log(`O resultado da operação é: ${result}`);
 }
