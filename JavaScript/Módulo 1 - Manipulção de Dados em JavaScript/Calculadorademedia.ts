e8b78ec4c1ac2ff3381b7feb31b6b75f78da34b5
carloslsantana87 <139463823+carloslsantana87@users.noreply.github.com>
2023-11-20 16:34:04 -0300

    Atualizacao

---
+++ JavaScript/Módulo 1 - Manipulção de Dados em JavaScript/Calculadorademedia.ts
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')();
+
+var nota1:number, nota2:number, nota3:number, media:number; 
+
+console.log(`
+         CALCULO DE MÉDIA DO ALUNO!!!
+         `);
+
+nota1 = parseFloat(prompt(`Digite a primeira nota - de 0 a 10, considere também digitar os décimos:`));
+nota2 = parseFloat(prompt(`Digite a segunda nota - de 0 a 10, considere também digitar os décimos:`));
+nota3 = parseFloat(prompt(`Digite a terceira nota - de 0 a 10, considere também digitar os décimos:`));
+
+//cálculo da média
+
+media = (nota1+nota2+nota3)/3;
+
+console.log(`
+         RESULTADO DO CALCULO !!!
+         `);
+
+console.log(`A média do aluno obtida pela soma das notas: ${nota1} + ${nota2} + ${nota3} é igual a ${media.toFixed(2)}`) ;
