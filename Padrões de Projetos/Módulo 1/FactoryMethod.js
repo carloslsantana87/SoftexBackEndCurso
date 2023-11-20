65c5dffbe9d8671bcceaf99f737cd139e191697a
carloslsantana87 <139463823+carloslsantana87@users.noreply.github.com>
2023-11-20 15:40:21 -0300

    Atualizacao

--- Padrões de Projetos/Módulo 1/FactoryMethod.js
+++ Padrões de Projetos/Módulo 1/FactoryMethod.js
@@ -1,12 +1,12 @@
 const prompt = require('prompt-sync')();
 
-class Computador{
-    constructor(ram, hdd, cpu, type,precocomputador){
+class Computador {
+    constructor(ram, hdd, cpu, type, precocomputador) {
         this.ram = ram + " " + "GB";
         this.hdd = "SSD" + " " + hdd + " " + "GB";
         this.cpu = cpu + " " + "GHZ";
         this.type = type;
-        this.precocomputador = "R$" + " " +precocomputador;
+        this.precocomputador = "R$" + " " + precocomputador;
     }
     dadosComputador() {
         return `\tMemória: ${this.ram} | HARD DISK: ${this.hdd}  | CPU: ${this.cpu} | PREÇO: ${this.precocomputador} \n`;
@@ -24,20 +24,20 @@ class Computador{
     }
 }
 class PC extends Computador {
-    constructor(ram, hdd, cpu, type, precocomputador){
-        super(ram, hdd, cpu, type,precocomputador);
-    } 
+    constructor(ram, hdd, cpu, type, precocomputador) {
+        super(ram, hdd, cpu, type, precocomputador);
+    }
 }
 class Server extends Computador {
-    constructor(ram, hdd, cpu, type, precocomputador){
+    constructor(ram, hdd, cpu, type, precocomputador) {
         super(ram, hdd, cpu, type, precocomputador);
     }
 }
 var tipoComputador = parseInt(prompt("Escolha qual o tipo de computador a ser vendido: 1 - Para PC, 2 - Para servdor:"));
 if (tipoComputador === 1) {
-    var comPracomputador = new PC(16,320,"Core I5 6.7", "PC", 3000);
-   console.log(comPracomputador.imprimirNotacomputador());
+    var comPracomputador = new PC(16, 320, "Core I5 6.7", "PC", 3000);
+    console.log(comPracomputador.imprimirNotacomputador());
 } else {
-    var comPracomputador = new Server(32,640,"Core I9 12.9", "Servidor", 15000);
-  console.log(comPracomputador.imprimirNotacomputador());
+    var comPracomputador = new Server(32, 640, "Core I9 12.9", "Servidor", 15000);
+    console.log(comPracomputador.imprimirNotacomputador());
 }
--- Padrões de Projetos/Módulo 1/Prototype.js
+++ Padrões de Projetos/Módulo 1/Prototype.js
@@ -4,23 +4,23 @@ class Automovel {
     constructor(marca, modelo, ano) {
         this.marca = "FIAT";
         this.modelo = "CHRONOS";
-        this.ano = 2024;             
+        this.ano = 2024;
     }
-    cloneObjDuas(){        
-        return `\tMARCA: ${this.marca} |  MODELO: ${this.modelo } | ANO: ${this.ano} | TIP TRILHO: ${this.tipotrilhobanco}  \n`;      
+    cloneObjDuas() {
+        return `\tMARCA: ${this.marca} |  MODELO: ${this.modelo} | ANO: ${this.ano} | TIP TRILHO: ${this.tipotrilhobanco}  \n`;
     }
-    cloneObjQuatro(){        
-        return `\tMARCA: ${this.marca} |  MODELO: ${this.modelo } | ANO: ${this.ano} | MULTIMIDIA: ${this.multimidia} \n`;          
+    cloneObjQuatro() {
+        return `\tMARCA: ${this.marca} |  MODELO: ${this.modelo} | ANO: ${this.ano} | MULTIMIDIA: ${this.multimidia} \n`;
     }
-    represent(){
-        if (this.multimidia === undefined){
+    represent() {
+        if (this.multimidia === undefined) {
             var notacomp = "\tREGISTRO DE FABRICAÇÃO DE VEÍCULO\n";
             notacomp += "\t--------------------------------------------------------------------\n";
             notacomp += "\tMARCA\t\t\tMODELO\t\t\tANO\t\t\t\n";
             notacomp += "\t--------------------------------------------------------------------\n";
             notacomp += this.cloneObjDuas();
             notacomp += "\t--------------------------------------------------------------------\n";
-            return notacomp;  
+            return notacomp;
         } else {
             var notacomp = "\tREGISTRO DE FABRICAÇÃO DE VEÍCULO\n";
             notacomp += "\t---------------------------------------------------------------------------\n";
@@ -28,69 +28,69 @@ class Automovel {
             notacomp += "\t---------------------------------------------------------------------------\n";
             notacomp += this.cloneObjQuatro();
             notacomp += "\t---------------------------------------------------------------------------\n";
-            return notacomp;  
+            return notacomp;
 
-        }  
- }
+        }
+    }
 }
 
-class Duasportas extends Automovel{
+class Duasportas extends Automovel {
     constructor(tipotrilhobanco) {
         super();
         this.tipotrilhobanco = tipotrilhobanco;
     }
 }
-class Quatroportas extends Automovel{
+class Quatroportas extends Automovel {
     constructor(multimidia) {
         super();
         this.multimidia = multimidia;
-      
+
     }
 }
 class Aplicacao extends Automovel {
-    constructor () {
+    constructor() {
         super();
         this.carrofabricado = [];
     }
-    addVeiculo(item){
-       this.carrofabricado.push(item)
-    }     
+    addVeiculo(item) {
+        this.carrofabricado.push(item)
+    }
 }
 
 //Veiculos duas portas
 var carroDuasportas = new Duasportas("fixo");
-var itemdeproducao  = new Aplicacao (carroDuasportas);
+var itemdeproducao = new Aplicacao(carroDuasportas);
 var addItemclonado = carroDuasportas.cloneObjDuas(itemdeproducao);
 
 var carroDuasportasUm = new Duasportas("removivel");
-var itemdeproducao  = new Aplicacao (carroDuasportasUm);
+var itemdeproducao = new Aplicacao(carroDuasportasUm);
 var addItemclonado = carroDuasportasUm.cloneObjDuas(itemdeproducao);
 
 var carroDuasportasDois = new Duasportas("hibrido");
-var itemdeproducao  = new Aplicacao (carroDuasportasDois);
+var itemdeproducao = new Aplicacao(carroDuasportasDois);
 var addItemclonado = carroDuasportasDois.cloneObjDuas(itemdeproducao);
 
 
 //Veiculos quatro portas
 var carroQuatroportas = new Quatroportas("Encosto do banco");
-var itemdeproducao  = new Aplicacao (carroQuatroportas);
+var itemdeproducao = new Aplicacao(carroQuatroportas);
 var addItemclonado = carroQuatroportas.cloneObjQuatro(itemdeproducao);
 
 var carroQuatroportasUm = new Quatroportas("Pain. e enc. do banco");
-var itemdeproducao  = new Aplicacao (carroQuatroportasUm);
+var itemdeproducao = new Aplicacao(carroQuatroportasUm);
 var addItemclonado = carroQuatroportasUm.cloneObjQuatro(itemdeproducao);
 
 var carroQuatroportasDois = new Quatroportas("Painel");
-var itemdeproducao  = new Aplicacao (carroQuatroportasDois);
+var itemdeproducao = new Aplicacao(carroQuatroportasDois);
 var addItemclonado = carroQuatroportasDois.cloneObjQuatro(itemdeproducao);
 
-itemdeproducao.addVeiculo(carroDuasportas); 
+itemdeproducao.addVeiculo(carroDuasportas);
 itemdeproducao.addVeiculo(carroDuasportasUm);
-itemdeproducao.addVeiculo(carroDuasportasDois);  
+itemdeproducao.addVeiculo(carroDuasportasDois);
 
-itemdeproducao.addVeiculo(carroQuatroportas); 
-itemdeproducao.addVeiculo(carroQuatroportasUm); 
-itemdeproducao.addVeiculo(carroQuatroportasDois); 
+itemdeproducao.addVeiculo(carroQuatroportas);
+itemdeproducao.addVeiculo(carroQuatroportasUm);
+itemdeproducao.addVeiculo(carroQuatroportasDois);
 
 console.log(carroDuasportas.represent());
 console.log("\n");
